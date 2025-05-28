bfb086a0e5934a5d0a89b1083fa845d97fbbbc7a
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-05-29 02:02:53 +0300

    style(Toggler): migrate component's styles to Tailwind CSS

--- src/components/Toggler.jsx
+++ src/components/Toggler.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import "./Toggler.css";
+// import "./Toggler.css";
 
 const Toggler = ({ value, valueList, useAction }) => {
   return (
-    <button className="toggler-btn" onClick={() => useAction()}>
+    <button
+      className="
+    text-3xl
+    border-0
+    w-12
+    h-12
+    rounded-full
+    bg-[var(--element-background-color)]
+    hover:bg-[var(--highlight-color)]
+    hover:transition-all
+    hover:duration-700
+    hover:ease-in-out
+    "
+      onClick={() => useAction()}
+    >
       {valueList[value]}
     </button>
   );
